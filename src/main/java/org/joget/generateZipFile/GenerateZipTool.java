020d27c4a7ddd33135a8349be0ad2c25138071eb
miaoxng <miaoxuan.ng@joget.com>
2024-05-18 03:24:05 +0800

    Modified: format document indentation, update version and add sample app

--- src/main/java/org/joget/generateZipFile/GenerateZipTool.java
+++ src/main/java/org/joget/generateZipFile/GenerateZipTool.java
@@ -23,17 +23,18 @@ import net.lingala.zip4j.model.enums.EncryptionMethod;
 import org.joget.apps.app.service.AppPluginUtil;
 
 import org.joget.commons.util.LogUtil;
+
 /**
  *
  * @author Maxson
  */
-public class GenerateZipTool extends DefaultApplicationPlugin{
-    
+public class GenerateZipTool extends DefaultApplicationPlugin {
+
     private final static String MESSAGE_PATH = "message/form/GenerateZipTool";
 
     @Override
     public Object execute(Map props) {
-        
+
         String recordId = getPropertyString("formRecordId");
         String formDefId = getPropertyString("formDefId");
         String formField = getPropertyString("formField");
@@ -42,133 +43,131 @@ public class GenerateZipTool extends DefaultApplicationPlugin{
         String targetRecordId = getPropertyString("targetRecordId");
         String zipFileName = getPropertyString("zipFileName");
         String password = getPropertyString("zipPassword");
-        
+
         ApplicationContext ac = AppUtil.getApplicationContext();
         AppService appService = (AppService) ac.getBean("appService");
         AppDefinition appDef = AppUtil.getCurrentAppDefinition();
-        
-        String path = FileUtil.getUploadPath( appService.getFormTableName(appDef, targetFormId) , targetRecordId);
-//        zipFileName = path + zipFileName + ".zip"; 
+
+        String path = FileUtil.getUploadPath(appService.getFormTableName(appDef, targetFormId), targetRecordId);
+        // zipFileName = path + zipFileName + ".zip";
         zipFileName += ".zip";
         File newZipFile = new File(path + zipFileName);
-        
-        try{
-            FormRowSet set = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), formDefId, recordId);
+
+        try {
+            FormRowSet set = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), formDefId,
+                    recordId);
             FormRow row = set.get(0);
-            
-            //Get excel file
-            if(!row.get(getPropertyString("formField")).toString().isEmpty()){
-                //Get files name
-                String filesName = row.get(getPropertyString("formField")).toString();
+
+            // Get excel file
+            if (!row.get(formField).toString().isEmpty()) {
+                // Get files name
+                String filesName = row.get(formField).toString();
                 String[] fileNames = filesName.split(";");
-                
-                //Validate if password is provided 
-                if(!password.isEmpty()){
+
+                // Validate if password is provided
+                if (!password.isEmpty()) {
                     LogUtil.info(getClassName(), "Password provided");
-                    
-                    //Define Zip Parameters
+
+                    // Define Zip Parameters
                     ZipParameters zipParameters = new ZipParameters();
                     zipParameters.setEncryptFiles(true);
                     zipParameters.setEncryptionMethod(EncryptionMethod.AES);
-                    
-                    try{
-                        ZipFile zipFile = new ZipFile(newZipFile , password.toCharArray());                        
-                        
-                        
+
+                    try {
+                        ZipFile zipFile = new ZipFile(newZipFile, password.toCharArray());
+
                         // Add files to the zip file
                         for (String fileName : fileNames) {
-                            File file = FileUtil.getFile(fileName.trim(), appService.getFormTableName(appDef, formDefId), recordId);
+                            File file = FileUtil.getFile(fileName.trim(),
+                                    appService.getFormTableName(appDef, formDefId), recordId);
                             if (file != null && file.exists()) {
                                 zipFile.addFile(file, zipParameters);
                             } else {
                                 LogUtil.info(getClassName(), "File not found: " + fileName);
                             }
                         }
-                            LogUtil.info(getClassName(), "Files have been zipped successfully!");
-                        
-                            //get target form row
-                            FormRowSet targetSet = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetRecordId);
-                            FormRow targetRow = targetSet.get(0); 
+                        LogUtil.info(getClassName(), "Files have been zipped successfully!");
 
-                            //Add Zip File into selected form field
-                            if(!targetRow.get(getPropertyString("targetFormField")).toString().isEmpty()){
-                                targetRow.put(getPropertyString("targetFormField"), targetRow.get(getPropertyString("targetFormField")) + ";" + zipFileName);
-                            }else{
-                                targetRow.put(getPropertyString("targetFormField"), zipFileName);
-                            }
-                            targetSet.remove(0); 
-                            targetSet.add(0, targetRow);
-                            appService.storeFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetSet, targetRecordId);
-                            LogUtil.info(getClassName(), "Zip File Generated Successfully for [" + targetRecordId + "]");                    
+                        // get target form row
+                        FormRowSet targetSet = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetRecordId);
+                        FormRow targetRow = targetSet.get(0);
 
-                    }catch(Exception e){
+                        // Add Zip File into selected form field
+                        if (!targetRow.get(targetFormField).toString().isEmpty()) {
+                            targetRow.put(targetFormField, targetRow.get(targetFormField) + ";" + zipFileName);
+                        } else {
+                            targetRow.put(targetFormField, zipFileName);
+                        }
+                        targetSet.remove(0);
+                        targetSet.add(0, targetRow);
+                        appService.storeFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetSet, targetRecordId);
+                        LogUtil.info(getClassName(), "Zip File Generated Successfully for [" + targetRecordId + "]");
+
+                    } catch (Exception e) {
                         LogUtil.error("Zip File (password)", e, e.getMessage());
                     }
-                    
-                }else{
-                    LogUtil.info(getClassName(), "No password provided");                    
 
-                    //Zip all excel files into 1 zip file
-                    try (FileOutputStream fos = new FileOutputStream(newZipFile);
-                        ZipOutputStream zos = new ZipOutputStream(fos)) {
+                } else {
+                    LogUtil.info(getClassName(), "No password provided");
+
+                    // Zip all excel files into 1 zip file
+                    try (FileOutputStream fos = new FileOutputStream(newZipFile); ZipOutputStream zos = new ZipOutputStream(fos)) {
 
                         // Buffer for reading the file
                         byte[] buffer = new byte[1024];
 
-                            // Iterate over each file name
-                            for (String fileName : fileNames) {
-                                // Get the file
-                                File file = FileUtil.getFile(fileName.trim(), appService.getFormTableName(appDef, formDefId), recordId);
+                        // Iterate over each file name
+                        for (String fileName : fileNames) {
+                            // Get the file
+                            File file = FileUtil.getFile(fileName.trim(),
+                                    appService.getFormTableName(appDef, formDefId), recordId);
 
-                                if (file != null && file.exists()) {
-                                    // Add a new entry to the zip file
-                                    ZipEntry zipEntry = new ZipEntry(fileName.trim());
-                                    zos.putNextEntry(zipEntry);
+                            if (file != null && file.exists()) {
+                                // Add a new entry to the zip file
+                                ZipEntry zipEntry = new ZipEntry(fileName.trim());
+                                zos.putNextEntry(zipEntry);
 
-                                    // Read the file and write it to the zip output stream
-                                    try (FileInputStream fis = new FileInputStream(file)) {
-                                        int length;
-                                        while ((length = fis.read(buffer)) > 0) {
-                                            zos.write(buffer, 0, length);
-                                        }
+                                // Read the file and write it to the zip output stream
+                                try (FileInputStream fis = new FileInputStream(file)) {
+                                    int length;
+                                    while ((length = fis.read(buffer)) > 0) {
+                                        zos.write(buffer, 0, length);
                                     }
-                                    // Close the current entry
-                                    zos.closeEntry();
-                                } else {
-                                    LogUtil.info(getClassName(), "File not found: " + fileName);
                                 }
+                                // Close the current entry
+                                zos.closeEntry();
+                            } else {
+                                LogUtil.info(getClassName(), "File not found: " + fileName);
                             }
-                            LogUtil.info(getClassName(), "Files have been zipped successfully!");
+                        }
+                        LogUtil.info(getClassName(), "Files have been zipped successfully!");
 
+                        // get target form row
+                        FormRowSet targetSet = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetRecordId);
+                        FormRow targetRow = targetSet.get(0);
 
-                            //get target form row
-                            FormRowSet targetSet = appService.loadFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetRecordId);
-                            FormRow targetRow = targetSet.get(0); 
-
-                            //Add Zip File into selected form field
-                            if(!targetRow.get(getPropertyString("targetFormField")).toString().isEmpty()){
-                                targetRow.put(getPropertyString("targetFormField"), targetRow.get(getPropertyString("targetFormField")) + ";" + zipFileName);
-                            }else{
-                                targetRow.put(getPropertyString("targetFormField"), zipFileName);
-                            }
-                            targetSet.remove(0); 
-                            targetSet.add(0, targetRow);
-                            appService.storeFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetSet, targetRecordId);
-                            LogUtil.info(getClassName(), "Zip File Generated Successfully for [" + targetRecordId + "]");
-                        } catch (IOException e) {
-                            LogUtil.error("Zip File (no-password)", e, e.getMessage());
-                        } 
+                        // Add Zip File into selected form field
+                        if (!targetRow.get(targetFormField).toString().isEmpty()) {
+                            targetRow.put(targetFormField, targetRow.get(targetFormField) + ";" + zipFileName);
+                        } else {
+                            targetRow.put(targetFormField, zipFileName);
+                        }
+                        targetSet.remove(0);
+                        targetSet.add(0, targetRow);
+                        appService.storeFormData(appDef.getAppId(), appDef.getVersion().toString(), targetFormId, targetSet, targetRecordId);
+                        LogUtil.info(getClassName(), "Zip File Generated Successfully for [" + targetRecordId + "]");
+                    } catch (IOException e) {
+                        LogUtil.error("Zip File (no-password)", e, e.getMessage());
                     }
-    
-                } else {
-                    LogUtil.info(getClassName(), "Form Row not found for ID: " + recordId);
                 }
-            
-        } catch(Exception e){
+
+            } else {
+                LogUtil.info(getClassName(), "Form Row not found for ID: " + recordId);
+            }
+
+        } catch (Exception e) {
             LogUtil.error("App - recordID", e, "Failed to generate Zip File for [" + targetRecordId + "]");
         }
-        
-        
         return null;
     }
 
@@ -184,7 +183,7 @@ public class GenerateZipTool extends DefaultApplicationPlugin{
 
     @Override
     public String getDescription() {
-        return AppPluginUtil.getMessage("org.joget.GenerateZipTool.pluginDesc" , getClassName() , MESSAGE_PATH);
+        return AppPluginUtil.getMessage("org.joget.GenerateZipTool.pluginDesc", getClassName(), MESSAGE_PATH);
     }
 
     @Override
@@ -199,8 +198,7 @@ public class GenerateZipTool extends DefaultApplicationPlugin{
 
     @Override
     public String getPropertyOptions() {
-        return AppUtil.readPluginResource(getClassName(), "/properties/form/generateZipTool.json", null, true, MESSAGE_PATH);
+        return AppUtil.readPluginResource(getClassName(), "/properties/form/generateZipTool.json", null, true,
+                MESSAGE_PATH);
     }
-    
-    
 }
